2e2399d94fc19654474731446181acc393489973
leona <leona@DESKTOP-65JM4NN>
2025-03-02 11:23:53 -0300

    Adicionar arquivos de projeto.

---
+++ ContadorApp/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace ContadorApp
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ ContadorApp/MainPage.xaml.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.Maui.Storage; // Importante para salvar dados localmente
+
+namespace ContadorApp
+{
+    public partial class MainPage : ContentPage
+    {
+        int contador1 = 0;
+        int contador2 = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+
+            // Carregar os últimos valores salvos
+            if (Preferences.ContainsKey("contador1"))
+            {
+                contador1 = Preferences.Get("contador1", 0);
+                contadorLabel1.Text = contador1.ToString();
+            }
+
+            if (Preferences.ContainsKey("contador2"))
+            {
+                contador2 = Preferences.Get("contador2", 0);
+                contadorLabel2.Text = contador2.ToString();
+            }
+        }
+
+        private void OnIncrementClicked1(object sender, EventArgs e)
+        {
+            contador1++;
+            AtualizarContador();
+        }
+
+        private void OnDecrementClicked1(object sender, EventArgs e)
+        {
+            contador1--;
+            AtualizarContador();
+        }
+
+        private void OnIncrementClicked2(object sender, EventArgs e)
+        {
+            contador2++;
+            AtualizarContador();
+        }
+
+        private void OnDecrementClicked2(object sender, EventArgs e)
+        {
+            contador2--;
+            AtualizarContador();
+        }
+
+        private void OnResetClicked(object sender, EventArgs e)
+        {
+            contador1 = 0;
+            contador2 = 0;
+            AtualizarContador();
+        }
+
+        private void AtualizarContador()
+        {
+            contadorLabel1.Text = contador1.ToString();
+            contadorLabel2.Text = contador2.ToString();
+            Preferences.Set("contador1", contador1); // Salvar o valor
+            Preferences.Set("contador2", contador2);
+        }
+    }
+}
